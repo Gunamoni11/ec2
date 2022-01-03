1ae0059911e3028ee1289b3efcfb06e5bb9c0224
Naresh Kumar G <93704634+Gunamoni11@users.noreply.github.com>
2022-01-03 10:01:18 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,7 @@
-{
-  region = "us-east-2"
-}
 resource "aws_instance" "ec2" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+  region = "us-east-2"
   subnet_id   = "subnet-0017b80df448722c1"
   key_name = "jenkins"
   security_groups = ["sg-0862e0d8235267a61"]
